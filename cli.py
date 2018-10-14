934d6382057c71cecb49c150c190869c372a0256
dosisod <39638017+dosisod@users.noreply.github.com>
2018-10-13 23:36:24 -0700

    added file indexing

--- cli.py
+++ cli.py
@@ -2,7 +2,8 @@ from grit import grit
 
 def main():
 	gg=grit("config/")
-	gg.merge([[1,2,3,4],[1,1,1,1]])
+	#gg.merge([[1,2,3,4],[1,1,1,1]])
+	gg.add("test.py","123","321")
 	gg.exit()
 
 if __name__=="__main__":
--- grit.py
+++ grit.py
@@ -1,15 +1,20 @@
 import os
 import hashlib
+import zipfile
 import requests
 from simplecsv import simplecsv
 
 class grit:
 	def __init__(self, dir):
 		self.dir=dir #folder storing IPs and index
+		self.home=os.getcwd() #stores where this file is ran from
 		if not os.path.exists(dir):
 			os.makedirs(dir)
 
-		self.ipsc=simplecsv(self.dir+"ips", "r+", delim=".") #IP simplecsv obj
+		self.indexsc=simplecsv(self.dir+"index", "w+") #reads index csv
+		self.index=self.indexsc.table
+
+		self.ipsc=simplecsv(self.dir+"ips", "w+", delim=".") #IP simplecsv obj
 		self.ips=[]
 		for i in self.ipsc.table: #array from csv file
 			temp=[] #temp array for storing values
@@ -17,17 +22,25 @@ class grit:
 				temp.append(int(j)) #IPs must be converted into ints
 			self.ips.append(temp) #appends IP to array
 
-	def prep(self): #preps file?
-		pass
+	def add(self, file, name, desc): #adds hashes and other info to index file
+		row=[]
+		with open(file, "r") as f:
+			row.append(hashlib.sha512(f.read().encode()).hexdigest()) #saves file hash and full path to csv
+			row.append(os.path.realpath(f.name))
+
+		row.append(name)
+		row.append(desc)
+		self.indexsc.writerow(row) #push data to index csv
 
 	def download(self):
 		pass
 
-	def upload(self): #will upload file
+	def upload(self, file, ip): #will upload file
 		pass
 
 	def zip(self, input, output): #zips up a file
-		pass
+		with zipfile.ZipFile(output, "w", zipfile.ZIP_DEFLATED) as z:
+			z.write(input)
 
 	def select(): #file(s) to upload
 		pass
